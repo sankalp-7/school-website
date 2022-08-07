4a6132e1780c34eb13816e18afd6aac39a940932
Sankalp <104098061+sankalp-7@users.noreply.github.com>
2022-08-07 12:18:22 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,15 +1,22 @@
 
-#function to get remainder dictionary
+from decimal import Decimal
 
 def give_remainder_dict(a,b,c):
-    x=a/b
-    f=round(x,c)    #rounding off numbers up to c deacimals
-    f=str(f)
-    s=f.split(".") # if f=1.234 then s=[1,234]
-    num=str(s[1])
-    d={str(k):0 for k in range(10)} #intitalizing dict keys from range 0-9
+    x=Decimal(a)/Decimal(b)  #we use decimal model in order to get upto 28 places after decimal. In default float python can only accomodate upto 18 places
+    f=str(x)                 #converting x to string in order to perform string operations
+    s=f.split(".")           #if f="1.234" then s=['1','234']
+    num=str(s[1])             
+    if c<=28:
+        num=num[:c]          #req number of decimal places is less then 28 then we slice our string to the req number of digits
+    if num.count(num[0])==len(num):  #special case for divisions where all the decimal places are equal (eg:-1/3) and the req number of digits is > 28
+        req=num[0]
+        if c>28:
+            diff=c-28
+            for i in range(diff):
+                num=num+req    #add that number to num diff number of times
+    d={str(k):0 for k in range(10)}
     for n in num:
-        d[n]+=1 #increasing dict key value for every digit in num
+        d[n]+=1    #increment value in dict for every key found in num
     return d
 
 
