af1689aeac4ca47b81a07ee17330367b3afcb75c
Sankalp <104098061+sankalp-7@users.noreply.github.com>
2022-08-07 11:56:35 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,15 +1,15 @@
 
-
+#function to get remainder dictionary
 
 def give_remainder_dict(a,b,c):
     x=a/b
-    f=round(x,c)
+    f=round(x,c)    #rounding off numbers up to c deacimals
     f=str(f)
-    s=f.split(".")
+    s=f.split(".") # if f=1.234 then s=[1,234]
     num=str(s[1])
-    d={str(k):0 for k in range(10)}
+    d={str(k):0 for k in range(10)} #intitalizing dict keys from range 0-9
     for n in num:
-        d[n]+=1
+        d[n]+=1 #increasing dict key value for every digit in num
     return d
 
 
