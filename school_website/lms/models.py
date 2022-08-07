c5b2af8b61cbe7e0c09b1f2446fbd9047cf077ce
Sankalp <104098061+sankalp-7@users.noreply.github.com>
2022-08-07 11:52:40 +0530

    Update models.py

--- school_website/lms/models.py
+++ school_website/lms/models.py
@@ -4,15 +4,19 @@ from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User,AbstractUser
 # Create your models here.
+#extending the base User model with two new fields using abstract user
 class User(AbstractUser):
     is_student=models.BooleanField(default=False)
     is_teacher=models.BooleanField(default=False)
+   #student model
 class student(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE,primary_key = True)
     standard=models.IntegerField()
     section=models.CharField(max_length=1)
     stream=models.CharField(max_length=100)
     roll_no=models.IntegerField()
+    
+   #teacher model
 class teacher(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE,primary_key = True)
     subject=models.CharField(max_length=100)
