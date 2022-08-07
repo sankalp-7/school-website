f58a9d660338dfd26d04edd3928100852d20f733
Sankalp <104098061+sankalp-7@users.noreply.github.com>
2022-08-07 11:53:15 +0530

    Update admin.py

--- school_website/lms/admin.py
+++ school_website/lms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from.models import student
+from.models import student,teacher
 # Register your models here.
 admin.site.register(student)
+admin.site.register(teacher)
